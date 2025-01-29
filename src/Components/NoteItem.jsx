ef623156f60b486b6fa711f08cced77a61906f29
Sappy2001 <saptangsumodak@gmail.com>
2025-01-29 13:59:39 +0530

    added-fontAwesome react package and imported fonts

--- src/Components/NoteItem.jsx
+++ src/Components/NoteItem.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faPenToSquare,
+	faRectangleXmark,
+} from "@fortawesome/free-solid-svg-icons";
 const NoteItem = ({ note }) => {
 	return (
 		<div className="col-md-3">
 			<div className="card m-3">
-				<div className="card-body">
+				<div className="container d-flex justify-content-between w-100 my-2">
+					<button type="button" class="btn ">
+						<FontAwesomeIcon style={{ color: "red" }} icon={faRectangleXmark} />
+					</button>
+					<button type="button" class="btn  ">
+						<FontAwesomeIcon style={{ color: "blue" }} icon={faPenToSquare} />
+					</button>
+				</div>
+				<div className="card-body container">
 					<h5 className="card-title">{note.title}</h5>
 					<p className="card-text">{note.description}</p>
-					<a href="#" className="btn btn-primary">
-						Go somewhere
-					</a>
 				</div>
 			</div>
 		</div>
